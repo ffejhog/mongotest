b80d46adc50ea639c78e7ba5fc362d95ae7ef4bc
Jeffrey Neer <ffejhog@users.noreply.github.com>
2018-01-23 22:51:07 -0600

    Some init stuff

---
+++ UTDDesignMongoDemo/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UTDDesignMongoDemo.Models;
+
+namespace UTDDesignMongoDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Save()
+        {
+            ViewData["Message"] = "Save stuff here";
+
+            return View();
+        }
+
+        public IActionResult Read()
+        {
+            ViewData["Message"] = "Read Stuff here";
+
+            return View();
+        }
+
+        public IActionResult SaveGood()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ UTDDesignMongoDemo/Controllers/UploadFileController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UTDDesignMongoDemo.Controllers
+{
+    public class UploadFileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("UploadFile")]
+        [RequestSizeLimit(104857600)]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+            long size = file.Length;
+
+            //Get a place to store the temp file on the server
+            var filePath = Path.GetTempFileName();
+
+            if (file.Length > 0)
+            {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+            }
+
+            //Do stuff with file into mongo
+
+            return RedirectToAction("SaveGood", "Home");
+        }
+
+
+    }
+}
